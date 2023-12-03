98e717bd82133eb666bce49ce6965176f18554ea
Darcy Cox <darcycox97@gmail.com>
2023-12-03 18:04:37 +1100

    day 1

---
+++ day_01/solution.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <map>
+#include <set>
+
+namespace aoc
+{
+
+    namespace
+    {
+
+        const std::array<std::string, 9> DIGITS_AS_TEXT{
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"};
+
+        const int MAX_DIGIT_LEN = 5;
+
+    }
+
+    void solve1_part1(std::ifstream &input)
+    {
+        std::string line;
+        int sum = 0;
+        while (std::getline(input, line))
+        {
+            int d1 = *std::find_if(line.begin(), line.end(), [](char c)
+                                   { return isdigit(c); }) -
+                     '0';
+            int d2 = *std::find_if(line.rbegin(), line.rend(), [](char c)
+                                   { return isdigit(c); }) -
+                     '0';
+
+            int combined = d1 * 10 + d2;
+            sum += combined;
+
+            printf("first digit: %d. second digit: %d. combined: %d\n", d1, d2, combined);
+        }
+
+        std::cout << sum << std::endl;
+    }
+
+    void solve1_part2(std::ifstream &input)
+    {
+        // same as above but the first "digit" can be spelt out with text.
+
+        // DIGITS_AS_TEXT, and MAX_DIGIT_LEN
+
+        std::string line;
+        int sum = 0;
+        while (getline(input, line))
+        {
+            int firstDigit = -1;
+            int lastDigit = -1;
+
+            for (int i = 0; i < line.size(); ++i)
+            {
+                int d = -1;
+                if (isdigit(line[i]))
+                {
+                    d = line[i] - '0';
+                }
+                else
+                {
+                    for (int j = i + 1; j < line.size() && j - i + 1 <= MAX_DIGIT_LEN; ++j)
+                    {
+                        std::string candidate = line.substr(i, j - i + 1);
+                        const auto it = std::find(DIGITS_AS_TEXT.begin(), DIGITS_AS_TEXT.end(), candidate);
+                        if (it != DIGITS_AS_TEXT.end())
+                        {
+                            d = std::distance(DIGITS_AS_TEXT.begin(), it) + 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (d != -1)
+                {
+                    if (firstDigit == -1)
+                        firstDigit = d;
+                    lastDigit = d;
+                }
+            }
+
+            if (firstDigit < 1 || lastDigit < 1)
+                throw std::runtime_error("bad input!");
+
+            const int combined = firstDigit * 10 + lastDigit;
+            printf("first digit: %d. second digit: %d. combined: %d\n", firstDigit, lastDigit, combined);
+            sum += combined;
+        }
+
+        printf("total: %d\n", sum);
+    }
+
+}
---
+++ main.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+// #include "nest/test.h"
+
+#include <fstream>
+#include "day_01/solution.h"
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "Usage: " << argv[0] << " <day> <variant(1|2)> <path_to_input>" << std::endl;
+        return 1;
+    }
+
+    const int day = std::stoi(argv[1]);
+    const int variant = std::stoi(argv[2]);
+    const char *input_filename = argv[3];
+
+    printf("Running day %d part %d with input file %s\n", day, variant, input_filename);
+
+    std::ifstream input_file;
+    input_file.open(input_filename, std::ios::in);
+
+    if (day == 1)
+    {
+        if (variant == 1)
+            aoc::solve1_part1(input_file);
+        else
+            aoc::solve1_part2(input_file);
+    }
+
+    input_file.close();
+
+    return 0;
+}
